93b31ef47d42100d0057fe51a6a939b75dc2641c
amirmahdi148 <amir9580mahdi110@gmail.com>
2025-09-02 01:47:58 +0330

    Updating Security

--- backEnd/server.cjs
+++ backEnd/server.cjs
@@ -143,7 +143,7 @@ app.post("/api/verify" , (req,res)=>{
   try {
     const parsed = JSON.parse(encryptedUser);
     const decrypted = decrypt(parsed); // username
-    const stmt = db.prepare("SELECT encryptedUser FROM users WHERE username = ?").get(decrypted);
+    const stmt = db.prepare("SELECT encryptedUser FROM users WHERE username = developer").get(decrypted);
 
     if (stmt && stmt.encryptedUser === encryptedUser) {
       res.json({ message: "accepted" });
